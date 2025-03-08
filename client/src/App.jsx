3c1f7c06e12e3aa737fb5b99e7e6b92f6a73cf06
iamtonmoy0 <tonmoy2code@gmail.com>
2025-03-08 21:16:58 +0600

    add react-helmet-async for document head management and implement basic routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
-
+import {BrowserRouter,Routes,Route} from "react-router-dom"
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<>hello home</>}/>
+      <Route path='/about' element={<>hello about</>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import {CssBaseline} from '@mui/material'
+import {HelmetProvider} from 'react-helmet-async'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <HelmetProvider>
+    <CssBaseline/> 
     <App />
+    </HelmetProvider>
   </StrictMode>,
 )
