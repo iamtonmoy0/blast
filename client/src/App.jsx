208ec8fdf20994072d012878ef9f9caee6428278
iamtonmoy0 <tonmoy2code@gmail.com>
2025-03-08 21:26:17 +0600

    implement lazy loading for routes and add Home, Login, Chat, and Group pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React,{lazy} from 'react'
 import {BrowserRouter,Routes,Route} from "react-router-dom"
+
+
+const Home = lazy(()=>import('./pages/Home'))
+const Login = lazy(()=>import('./pages/Login'))
+const Chat = lazy(()=>import('./pages/Chat'))
+const Group = lazy(()=>import('./pages/Group'))
 export default function App() {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path='/' element={<>hello home</>}/>
-      <Route path='/about' element={<>hello about</>}/>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/chat' element={<Chat/>}/>
+      <Route path='/group' element={<Group/>}/>
     </Routes>
     </BrowserRouter>
   )
