e833db26c887241dbae12d4050bfc393e6ba2620
iamtonmoy0 <tonmoy2code@gmail.com>
2025-03-09 12:46:45 +0600

    refactor routing and enhance Login page layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,19 @@
-import React,{lazy} from 'react'
-import {BrowserRouter,Routes,Route} from "react-router-dom"
+import React, { lazy } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-
-const Home = lazy(()=>import('./pages/Home'))
-const Login = lazy(()=>import('./pages/Login'))
-const Chat = lazy(()=>import('./pages/Chat'))
-const Group = lazy(()=>import('./pages/Group'))
+const Home = lazy(() => import("./pages/Home"));
+const Login = lazy(() => import("./pages/Login"));
+const Chat = lazy(() => import("./pages/Chat"));
+const Group = lazy(() => import("./pages/Group"));
 export default function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/login' element={<Login/>}/>
-      <Route path='/chat' element={<Chat/>}/>
-      <Route path='/group' element={<Group/>}/>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/chat/:chatId" element={<Chat />} />
+        <Route path="/group" element={<Group />} />
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import { Container, Paper } from "@mui/material";
 export default function Login() {
+  const [isLogin, setIsLogin] = useState(true);
   return (
-    <div>Login</div>
-  )
+    <Container component={"main"} maxWidth={"sx"}>
+      <Paper
+        elevation={3}
+        style={{
+          padding: 4,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      ></Paper>
+    </Container>
+  );
 }
